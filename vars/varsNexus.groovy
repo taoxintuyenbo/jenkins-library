4eed1d849d0ef6b7ef4802d951e4ee33fcd3f7b2
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-16 21:32:11 +0700

    Update varsNexus.groovy

--- vars/varsNexus.groovy
+++ vars/varsNexus.groovy
@@ -2,7 +2,8 @@ def call() {
     def nexusProps = readProperties(file: 'nexus.properties')
     env.NEXUS_URL         = nexusProps.nexusUrl
     env.NEXUS_URL_DOCKER  = nexusProps.nexusUrlDocker
-    env.NEXUS_PRO_REPO    = nexusProps.nexusRepo
+    env.NEXUS_SNAPSHOTS_REPO = nexusProps.nexusRepoSnapshots
+    env.NEXUS_RELEASES_REPO = nexusProps.nexusRepoReleases
     env.NEXUS_GROUP       = nexusProps.nexusGroup
     env.NEXUS_CRED        = nexusProps.nexusCred
     env.NEXUS_ARTIFACT_ID = nexusProps.nexusPrjID
