d87cd4437643751626112b31a0179bfcb009aa1b
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-16 22:02:25 +0700

    Update varsNexus.groovy

--- vars/varsNexus.groovy
+++ vars/varsNexus.groovy
@@ -4,6 +4,7 @@ def call() {
     env.NEXUS_URL_DOCKER  = nexusProps.nexusUrlDocker
     env.NEXUS_SNAPSHOTS_REPO = nexusProps.nexusRepoSnapshots
     env.NEXUS_RELEASES_REPO = nexusProps.nexusRepoReleases
+    env.ARTIFACT_PATH = nexusProps.artifactPath
     env.NEXUS_GROUP       = nexusProps.nexusGroup
     env.NEXUS_CRED        = nexusProps.nexusCred
     env.NEXUS_ARTIFACT_ID = nexusProps.nexusPrjID
