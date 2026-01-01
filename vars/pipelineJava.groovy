3c9c53b0f272d3cf3f4070789e4446275001ef4d
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2026-01-01 21:07:43 +0700

    Update pipelineJava.groovy

--- vars/pipelineJava.groovy
+++ vars/pipelineJava.groovy
@@ -1,16 +1,36 @@
-def call(){
+def call() {
   node {
-    checkoutSCM()
-    loadTools()
-    varsNexus()
-    varsServer()
-    buildJava()
-    //unitTestJava()
-   // qualityCheck()
-    //qualityGate()
-    pushArtifactJava()
-    pushSnapshot()
-    deployJava()
-    healthCheck()
+    try {
+      if (env.BRANCH_NAME == 'release' || env.BRANCH_NAME.startsWith('uat')) {
+        checkoutSCM()
+        loadTools()
+        varsNexus()
+        varsServerJava()
+        buildJava()
+        unitTestJava()
+        qualityCheck()
+        qualityGate()
+        pushArtifactJava()
+        pushSnapshot()
+        deployJava()
+        healthCheck()
+        notiSuccess()
+
+      } else if (env.BRANCH_NAME.startsWith('dev')) {
+        checkoutSCM()
+        loadTools()
+        buildJava()
+        unitTestJava()
+        qualityCheck()
+        qualityGate()
+        notiSuccess()
+      } else {
+        echo "No pipeline configured for branch: ${env.BRANCH_NAME}"
+      }
+
+    } catch (err) {
+      notiFail(stageLog: "${err}")
+      throw err
+    }
   }
 }
