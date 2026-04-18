54d46b1d2d244987fa1508b4fda41d88e04fbb3f
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2026-04-18 22:26:27 +0700

    Create pipelineTerraform.groovy

---
+++ vars/pipelineTerraform.groovy
@@ -0,0 +1,33 @@
+properties([
+  parameters([
+    choice(name: 'TF_ACTION', choices: ['init', 'plan', 'apply', 'destroy'], description: 'Terraform action to run'),
+  ])
+])
+
+node {
+  try {
+    deleteDir()
+    checkout scm
+
+    if (env.BRANCH_NAME == 'release' || env.BRANCH_NAME == 'main'){
+        dir("environments/prod"){
+            terraformRun(
+                action: params.TF_ACTION,
+                awsCred: env.AWS_CRED
+            )
+        }
+    } else if (env.BRANCH_NAME.startsWith('dev')){
+        dir("environments/dev") {
+            terraformRun(
+                action: params.TF_ACTION,
+                awsCred: env.AWS_CRED
+            )
+        }
+    }
+    notiSuccess()
+  }
+  catch (err) {
+      notiFail(stageLog: "${err}")
+      throw err
+    }
+}
